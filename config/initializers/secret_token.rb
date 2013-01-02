34c1fd507cd5ef3c60864fb669ddbe330c4cb871
Silas J. Matson <silas@clearsightstudio.com>
2013-01-02 18:45:04 +0000

    Initial: reproduced double forward slash.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestForwardSlash::Application.config.secret_token = '896245a526edcf58b1ef63b87b882bacd73e7c2ff64a83c81e18a51725413a35b55fa72a0c780549d1fd4b9460d4ea135f5229b4c285f48c48c2c154a23a4103'
